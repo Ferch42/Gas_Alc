f135f8892718935645a36763541f4beb8bb8e0a4
victor gomes de oliveira martins nicola <victor.nicola@usp.br>
2018-01-02 17:23:00 -0200

    birth of the app

---
+++ app/src/androidTest/java/alcgas/studio/com/gas_alc/ApplicationTest.java
@@ -0,0 +1,13 @@
+package alcgas.studio.com.gas_alc;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
